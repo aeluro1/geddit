66789bcdee5d2731b537844a83360febaf16e79f
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-04-08 07:13:40 -0400

    Fixed typo

--- download.py
+++ download.py
@@ -158,7 +158,7 @@ class Downloader:
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
-        if not any([file for file in dest.parent.glob("{dest.name}.*")]):
+        if not any([file for file in dest.parent.glob(f"{dest.name}.*")]):
             raise RuntimeError("Failed to download video")
 
     def getAlbum(self, urls, dest):
