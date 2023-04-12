e4f6fbde848596c1c578baa89358ffbfa7823e15
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-04-11 23:28:13 -0400

    Check in

--- tools/tests.py
+++ tools/tests.py
@@ -8,7 +8,10 @@ class Tests(unittest.Test):
     def test_image_download_success(self):
         url = "https://i.imgur.com/"
         response = requests.get(url)
-        
+
+    def test_imgur_gallery_urls(self):
+        pass
+
 
 
 if __name__ == "__main__":
