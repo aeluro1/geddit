2d311ce787a1d79af72d3fe83a41458bde2b4d6e
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-06-24 18:34:26 -0400

    Fix tests

--- tests/tests.py
+++ tests/tests.py
@@ -3,15 +3,15 @@ import requests
 
 import geddit
 
-class Tests(unittest.Test):
-    
+class Tests(unittest.TestCase):
+
     def test_image_download_success(self):
         url = "https://i.imgur.com/"
         response = requests.get(url)
+        self.assertEqual(response.status_code, 200)
 
     def test_imgur_gallery_urls(self):
         pass
-
 
 
 if __name__ == "__main__":
